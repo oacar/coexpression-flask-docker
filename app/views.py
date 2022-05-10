ffcfecca9db641f49b3d52e91e14d242d8b9dce7
Omer Acar <omeracar0@gmail.com>
2022-05-10 11:56:00 -0400

    added basic database functions for sqlite3

--- app/views.py
+++ app/views.py
@@ -1,12 +1,27 @@
 from app import app
 from flask import render_template
+from flask import url_for
 
 
 @app.route("/")
 def home():
-    return "<b>There has been a change</b>"
+    return "<b>Hello world changed</b>"
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
+    ##return f"Hello {name}"
 
 
 @app.route("/template")
 def template():
     return render_template("home.html")
+
+
+with app.test_request_context():
+    print(url_for("home"))
+    print(url_for("hello"))
+    print(url_for("hello", name="omer"))
+## print(url_for('profile', username='John Doe'))
